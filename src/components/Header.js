264f5e40f3c3fe00442f080472633b78a577456b
Gabriela Ventura <gabrielaleitev@gmail.com>
2022-10-31 19:23:52 -0300

    Finalização do requisito 2, criação do Header e renderização do mesmo na page Wallet

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Header extends Component {
   render() {
+    const { userEmail } = this.props;
     return (
-      <div>Header</div>
+      <header>
+        <div
+          data-testid="email-field"
+        >
+          User:
+          {' '}
+          {userEmail}
+
+          <p
+            data-testid="total-field"
+          >
+            Total:
+            {' '}
+            0
+            {' '}
+            <span
+              data-testid="header-currency-field"
+            >
+              BRL
+            </span>
+          </p>
+        </div>
+      </header>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = (state) => ({
+  userEmail: state.user.email,
+});
+
+Header.propTypes = {
+  userEmail: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Header);
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import Header from '../components/Header';
 
 class Wallet extends React.Component {
   render() {
-    return <div>HELLO! Now you are at wallet page</div>;
+    return (
+      <header>
+        <Header />
+      </header>
+    );
   }
 }
 
