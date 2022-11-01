709417e0c8a2c169d5fe38a59d5219676ab2fb54
Gabriela Ventura <gabrielaleitev@gmail.com>
2022-11-01 14:32:13 -0300

    Iniciando o walletForm

--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -3,7 +3,30 @@ import React, { Component } from 'react';
 class WalletForm extends Component {
   render() {
     return (
-      <div>WalletForm</div>
+      <form>
+        <label
+          htmlFor="expense-value"
+        >
+          Valor da Despesa:
+          <input
+            type="number"
+            data-testid="value-input"
+            name="expense-value"
+            placeholder="Valor Despesa"
+          />
+        </label>
+        <label
+          htmlFor="expense-description"
+        >
+          Descrição:
+          <input
+            type="text"
+            data-testid="description-input"
+            name="expense-description"
+            placeholder="Descrição Despesa"
+          />
+        </label>
+      </form>
     );
   }
 }
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -4,9 +4,10 @@ import Header from '../components/Header';
 class Wallet extends React.Component {
   render() {
     return (
-      <header>
+      <>
         <Header />
-      </header>
+        <Wallet />
+      </>
     );
   }
 }
